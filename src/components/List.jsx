b94a5db744e9d968e1ef598766942caa87b15dbc
Gameciel <christofer.calvin.pranata@gmail.com>
2023-03-02 22:30:42 +0700

    finish

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,25 +5,34 @@ import { Link, useLocation, useParams } from "react-router-dom";
 export default function List(props) {
 	const location = useLocation();
 	const propsData = location.state;
-	const toArr = [];
-
 	const [loaded, setLoaded] = useState(false);
+	const [datas, setDatas] = useState([]);
 
+	const toArr = [];
 	useEffect(() => {
 		for (let key in propsData) {
 			toArr.push(propsData[key].toString());
 		}
-
-		alert(`Datamu: \n${toArr}`);
-
+		setDatas([...toArr]);
 		setLoaded(true);
 	}, []);
 
+	return getPar(datas);
+}
+
+function getPar(props) {
 	return (
 		<>
-			<h1>Not yet</h1>
-			<p>{toArr}</p>
-			<Link to="/slide-07-exercises/">Click here to return</Link>
+			<div className="container">
+				<h1>Your Input:</h1>
+				{props.map((val, index) => (
+					<>
+						<b>{index + 1}:</b>
+						<li className="list-group-item mb-2">{val}</li>
+					</>
+				))}
+				<Link to="/slide-07-exercises/">Click here to return</Link>
+			</div>
 		</>
 	);
 }
