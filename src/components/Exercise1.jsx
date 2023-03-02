ac447633e1c718d5efd7c3ea41f5ffe98c9fe935
Gameciel <christofer.calvin.pranata@gmail.com>
2023-03-02 19:18:06 +0700

    finished exercise 1

--- src/components/Exercise1.jsx
+++ src/components/Exercise1.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
 
 export default class Exercise1 extends Component {
+	state = {
+		counter: 0,
+	};
+
+	incrementCounter = () => {
+		this.setState({ counter: this.state.counter + 1 });
+	};
+
+	decrementCounter = () => {
+		this.setState({ counter: this.state.counter - 1 });
+	};
+
+	resetCounter = () => {
+		this.setState({ counter: 0 });
+	};
+
 	render() {
 		return (
 			<div className="d-flex flex-row mt-5">
@@ -17,10 +33,33 @@ export default class Exercise1 extends Component {
 						</p>
 					</div>
 				</div>
-				<div
-					className="container mx-3 my-3 px-3 py-3"
-					style={{ backgroundColor: "#000000" }}
-				></div>
+				<div className="container mx-3 my-3 px-3 py-3 d-flex flex-column justify-content-center align-items-center">
+					<ul class="list-group mb-5">
+						<li class="list-group-item d-flex justify-content-between align-items-center">
+							Total count:
+							<span class="ml-2 badge badge-primary badge-pill">
+								{this.state.counter}
+							</span>
+						</li>
+					</ul>
+					<div>
+						<button onClick={this.decrementCounter} className="btn btn-danger">
+							{" "}
+							-{" "}
+						</button>
+						<button
+							onClick={this.resetCounter}
+							className="btn btn-warning ml-3 mr-3"
+						>
+							{" "}
+							Reset{" "}
+						</button>
+						<button onClick={this.incrementCounter} className="btn btn-success">
+							{" "}
+							+{" "}
+						</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
