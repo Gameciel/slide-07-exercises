ef34cdc07f564d8dc7dd69bb4809a5ec8407f2d5
Gameciel <christofer.calvin.pranata@gmail.com>
2023-03-04 23:48:58 +0700

    teori kep

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,30 @@ import Main from "./components/Main";
 import List from "./components/List";
 import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 function App() {
 	useEffect(() => {
 		let req = new XMLHttpRequest();
 		let time = new Date();
-		req.open(
-			"PUT",
-			"https://api.jsonbin.io/v3/b/640371ffebd26539d0891f86",
-			true
-		);
-		req.setRequestHeader("Content-Type", "application/json");
-		req.setRequestHeader(
-			"X-Master-Key",
-			"$2b$10$1OumGkPA7lQAOO.GfhNfvOmPvxfWvrVA2ZSD2NsZ7ZEzFkjwbpC3O"
-		);
-		req.send(`{"visit": "${time.toString()}"}`);
+		let prevData = [];
+
+		axios
+			.get(`https://api.jsonbin.io/v3/b/640371ffebd26539d0891f86`)
+			.then(res => {
+				prevData = [...res.data, time].split("");
+				req.open(
+					"PUT",
+					"https://api.jsonbin.io/v3/b/640371ffebd26539d0891f86",
+					true
+				);
+				req.setRequestHeader("Content-Type", "application/json");
+				req.setRequestHeader(
+					"X-Master-Key",
+					"$2b$10$1OumGkPA7lQAOO.GfhNfvOmPvxfWvrVA2ZSD2NsZ7ZEzFkjwbpC3O"
+				);
+				req.send(`{"visit": "${prevData}"`);
+			});
 	}, []);
 
 	return (
