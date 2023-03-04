5394355dfc837ed5db41ad61700ea8d31af8fd03
Gameciel <christofer.calvin.pranata@gmail.com>
2023-03-04 23:37:06 +0700

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Main from "./components/Main";
 import List from "./components/List";
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+	useEffect(() => {
+		let req = new XMLHttpRequest();
+		let time = new Date();
+		req.open(
+			"PUT",
+			"https://api.jsonbin.io/v3/b/640371ffebd26539d0891f86",
+			true
+		);
+		req.setRequestHeader("Content-Type", "application/json");
+		req.setRequestHeader(
+			"X-Master-Key",
+			"$2b$10$1OumGkPA7lQAOO.GfhNfvOmPvxfWvrVA2ZSD2NsZ7ZEzFkjwbpC3O"
+		);
+		req.send(`{"visit": "${time.toString()}"}`);
+	}, []);
+
 	return (
 		<>
 			<BrowserRouter>
